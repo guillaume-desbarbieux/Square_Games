eac1d04ade550dde89a6607abf93f305f0614654
guillaume-desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-10-19 16:14:06 +0200

    Add game menu loop for multiple game selections and quitting options

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,19 @@ import ui.View;
 public class Main {
     public static void main(String[] args) {
         View view = new View();
-        InteractionUser interact =  new InteractionUser(view);
+        InteractionUser interact = new InteractionUser(view);
+        int choice = 0;
 
-        view.displayTitle("Bienvenue sur Square GameType");
-        int choice = interact.getChoice("Choisissez un jeu", new String[]{"TicTacToe", "Puissance 4"});
-        switch (choice) {
-            case 1 -> new TicTacToe().start();
-            case 2 -> new Connect4().start();
-            default -> view.displayError("Choix invalide");
+        while (choice != 3) {
+            view.displayTitle("Bienvenue sur Square GameType");
+            choice = interact.getChoice("Choisissez un jeu", new String[]{"TicTacToe", "Puissance 4", "Quitter"});
+            switch (choice) {
+                case 1 -> new TicTacToe().start();
+                case 2 -> new Connect4().start();
+                case 3 -> view.display("A bientôt !");
+                default -> view.displayError("Choix invalide");
+            }
+            view.display("\n");
         }
-
     }
 }
